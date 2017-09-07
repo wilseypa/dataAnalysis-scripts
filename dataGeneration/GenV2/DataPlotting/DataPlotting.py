d6414cfaaceeb405436958d766dbc4d9cb20ecd0
Austin Reule <austinreule@gmail.com>
2017-09-07 10:48:25 -0400

    initial dataplotting commit

--- dataGeneration/GenV2/DataPlotting/DataPlotting.py
+++ dataGeneration/GenV2/DataPlotting/DataPlotting.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import inspect
+import numpy as np
+
 class DataPlotting():
     ''' A simple class for plotting the input data '''
     
@@ -5,35 +10,97 @@ class DataPlotting():
     def __init__(self, argDict):
         #TODO
         self.argDict = argDict
-        
+
     def test(self):
         print "Reached Test!"
-        return;
+        return
         
-    def generateDefaultPlots(self):
+    def generatePlots(self, fPathRaw, type):
         #TODO
+        plt.figure(1)
+        if type == 'pdf' or type == 'all':
+            plt.savefig(fPathRaw + '.pdf', bbox_inches='tight')
+        if type == 'png' or type == 'all':
+            plt.savefig(fPathRaw + '.png', bbox_inches='tight')
         return
     
     def clearPlots(self):
         #TODO
-        self.plt.clf()
+        plt.clf()
         return
     
     def showPlots(self):
         #TODO
-        self.plt.show()
+        plt.show()
         return
     
-    def r3DPlot(self):
+    def r3DPlot(self, xTestPts, yTestPts, zTestPts, ids, pos, cents, match):
         #TODO
+        x_val = [x for x in xTestPts]
+        y_val = [y for y in yTestPts]
+        z_val = [z for z in zTestPts]
+
+        fig = plt.figure(1)
+        ax = fig.add_subplot(pos, projection='3d')
+
+        markers = ['o', 'x']
+
+        colors = ['r', 'c', 'y', 'g', 'b', 'm', 'k']
+        for i in range(0, len(x_val)):
+            if (match == {}):
+                ax.scatter(x_val[i], y_val[i], z_val[i], c=colors[ids[i] % len(colors)], s=0.5)
+            else:
+                ax.scatter(x_val[i], y_val[i], z_val[i], marker=markers[match[i]], c=colors[ids[i] % len(colors)],
+                           s=0.5)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+
         return
     
-    def simplePlot(self):
+    def simplePlot(self,xTestPts, yTestPts, ids, cents, match):
         #TODO
+        self.clearPlots()
+        x_val = [x for x in xTestPts]
+        y_val = [x for x in yTestPts]
+
+        markers = ['o', 'x']
+        colors = ['r', 'c', 'y', 'g', 'b', 'm', 'k']
+
+        plt.figure(1)
+        plt.subplot(221)
+
+        for i in range(0, len(x_val)):
+            if (match == {}):
+                plt.scatter(x_val[i], y_val[i], c=colors[ids[i] % len(colors)], s=1)
+            else:
+                plt.scatter(x_val[i], y_val[i], marker=markers[match[i]], c=colors[ids[i] % len(colors)], s=1)
+
+        # minx, maxx = checkPlotBoundaries(x_val, minx, maxx)
+        # miny, maxy = checkPlotBoundaries(y_val, miny, maxy)
+
+        # plt.axis([minx,maxx,miny,maxy])
+
+        plt.grid(True, which='both')
         return
     
-    def eucPlot(self):
+    def eucPlot(self,testPts,ids,cents, match):
         #TODO
+        x_val = []
+        markers = ['o', 'x']
+        colors = ['r', 'c', 'y', 'g', 'b', 'm', 'k']
+        for row in testPts:
+            x_val.append([np.linalg.norm(row)])
+        plt.figure(1)
+        plt.subplot(223)
+
+        for i in range(0, len(x_val)):
+            if (match == {}):
+                plt.scatter(ids[i], x_val[i], c=colors[ids[i] % len(colors)], s=3)
+            else:
+                plt.scatter(ids[i], x_val[i], marker=markers[match[i]], c=colors[ids[i] % len(colors)], s=3)
+
+        plt.grid(True, which='both')
         return
     
         
--- dataGeneration/GenV2/testMain.py
+++ dataGeneration/GenV2/testMain.py
@@ -23,6 +23,15 @@ ts = TestSequences.TestSequences(argDict)
 dp = DataPlotting.DataPlotting(4)
 dp.test()
 
+if (len(data[0]) >= 2):
+    dp.simplePlot((x[0] for x in data), (x[1] for x in data), labels, cents, {});
+if (len(data[0]) >= 3):
+    dp.r3DPlot((x[0] for x in data), (x[1] for x in data), (x[2] for x in data), labels, 222, cents, {});
+if (len(data[0]) >= 6):
+    dp.r3DPlot((x[3] for x in data), (x[4] for x in data), (x[5] for x in data), labels, 224, cents, {});
+dp.eucPlot(data, labels, cents, {})
+dp.generatePlots("./", argDict["chartsOutput"])
+dp.showPlots()
 ut = utils.utils(argDict)
 ut.outputCents("./testoutput", cents)
 ut.outputRawData("./testoutput", data)
