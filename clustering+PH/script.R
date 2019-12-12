6fb08b1387fe6816dbb2a653ef55985c66442101
Anindya-Moitra <id.anindya@gmail.com>
2019-12-12 02:45:51 -0500

    Pushing a few upadtes, mostly to evaluate the stream clustering algorithm.

--- clustering+PH/script.R
+++ clustering+PH/script.R
@@ -1,9 +1,11 @@
 library(streamMOA)
 library(TDA)
+library(mclust)
 
-dataStream <- DSD_ReadCSV(file = "testData.csv", k = 14, class = 35, header = TRUE)
+dataStream <- DSD_ReadCSV(file = "testData.csv", k = 19, class = 35, header = TRUE)
+dataStreamWithLabels <- DSD_ReadCSV(file = "testData.csv", k = 19, header = TRUE)
 
-denStream <- DSC_DenStream(epsilon = 0.25, mu = 1, beta = 0.2, lambda = 0.01, initPoints = 100, offline = 2, processingSpeed = 1, recluster = TRUE)
+denStream <- DSC_DenStream(epsilon = 0.25, mu = 1, beta = 0.2, lambda = 0.01, initPoints = 200, offline = 20, processingSpeed = 1, recluster = TRUE)
 
 epoch <- 500
 pos <- 1
@@ -23,6 +25,12 @@ while ( dim( get_points(dataStream, n = 1, outofpoints = "ignore") )[1] != 0 ) {
   clusterLabels <- get_assignment( denStream, points, type = "macro" )
   points$clusterLabels <- clusterLabels
   
+  # For evaluation of clustering.
+  pointsWithLabels <- get_points( dataStreamWithLabels, n = epoch )
+  groundTruth <- pointsWithLabels$type
+  ari <- adjustedRandIndex(clusterLabels, groundTruth)
+  print(paste(t, ":", ari))
+  
   uniqueClusters <- unique(clusterLabels)
   
   pdf(file = paste0("output/time", t), width = 11, height = 8.50)
@@ -31,12 +39,12 @@ while ( dim( get_points(dataStream, n = 1, outofpoints = "ignore") )[1] != 0 ) {
     # assign(clustName, points[points$clusterLabels == clust, ])
     cluster <- points[points$clusterLabels == clust, ]
     cluster$clusterLabels <- NULL
-    Diag <- ripsDiag(X = cluster, maxDimension, eps, library = "GUDHI", printProgress = TRUE)
+    Diag <- ripsDiag(X = cluster, maxDimension, eps, library = "GUDHI", printProgress = FALSE)
     par(col.lab = "white")
     plot(Diag[["diagram"]], barcode = TRUE)
     par(col.lab = "black")
     title(main = paste("Barcodes: Cluster", clust), xlab = expression(epsilon))
-    legend("bottomright", inset = 0.08, legend = c(expression(H[0]), expression(H[1])), col = c("black", "red"), lty = 1, lwd = 1.5, cex = 0.9, box.lty = 0)
+    legend("right", inset = 0.08, legend = c(expression(H[0]), expression(H[1])), col = c("black", "red"), lty = 1, lwd = 1.5, cex = 0.9, box.lty = 0)
   }
   dev.off()
 }
